799d2b9bebd89eaf311f986dc0af43617bfec82c
rogerxu130 <rogerxu130@gmail.com>
2026-09-07 11:17:02 +1000

    fix: reject new weekly timeslots in archived terms

--- qas_custom/qas_custom/doctype/weekly_timeslot/weekly_timeslot.py
+++ qas_custom/qas_custom/doctype/weekly_timeslot/weekly_timeslot.py
@@ -1,5 +1,9 @@
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class WeeklyTimeslot(Document):
-	pass
+	def before_insert(self):
+		if self.term and frappe.db.get_value("Term", self.term, "status") == "Archived":
+			frappe.throw(_("Weekly timeslots cannot be added to an archived term. Choose another term."))
